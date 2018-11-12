14147e3cba53b640b5b34dc7d560a61bdfe660fc
Carlo Moro <cnmoro@gmail.com>
2018-11-12 13:43:04 -0200

    Uso de semáforos para leitura e envio da temperatura

--- rasp-socket-threads.py
+++ rasp-socket-threads.py
@@ -2,16 +2,19 @@ import socket
 import time
 from threading import Thread
 from random import randint
+import threading
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', 8089))
 serversocket.listen(5)
 
-lista_mensagens = []
+temperatura_atual = -999
 cooler_status = 0
 
 desktop_ip_found = 0
 desktop_ip_addr = ''
+
+semaforo = threading.Semaphore()
 
 def findServer():
     global desktop_ip_found
@@ -24,29 +27,30 @@ def findServer():
         indexVirg = ipstr.find(",")
         ipstr = ipstr[:indexVirg]
         ipstr = ipstr.replace("(", "").replace("'", "")
-        
+
         if len(buf) > 0:
             if buf == b'rasp?':
                 print('Servidor encontrado em ' + str(ipstr))
                 desktop_ip_found = 1
                 desktop_ip_addr = ipstr
                 respond('rasp!')
-                
-    #Thread 1 - Ler dados de temperatura do sensor
+
+    # Thread 1 - Ler dados de temperatura do sensor
     temperatureThread = Thread(target=readTemperature)
     temperatureThread.start()
 
-    #Thread 2 - Envio dos dados para o servidor
+    # Thread 2 - Envio dos dados para o servidor
     sendDataThread = Thread(target=sendData)
     sendDataThread.start()
 
-    #Thread 3 - Verificar novas mensagens do servidor
+    # Thread 3 - Verificar novas mensagens do servidor
     socketThread = Thread(target=connectFunc)
     socketThread.start()
 
-    #Thread 4 - Ativar e Desativar o cooler de resfriamento
+    # Thread 4 - Ativar e Desativar o cooler de resfriamento
     coolerThread = Thread(target=coolerHandler)
     coolerThread.start()
+
 
 def respond(msg):
     global desktop_ip_addr
@@ -56,34 +60,52 @@ def respond(msg):
     clientsocket.send(bytes(msg, 'UTF-8'))
     clientsocket.close()
 
+
 def connectFunc():
     while True:
-            connection, address = serversocket.accept()
-            buf = connection.recv(4096)
-            print('ip: ' + str(address))
-            if len(buf) > 0:
-                print('Recebido: ' + str(buf) + '\n')
-                respond('Mensagem Recebida pelo Rasp com Sucesso!')
-                
+        connection, address = serversocket.accept()
+        buf = connection.recv(4096)
+        print('ip: ' + str(address))
+        if len(buf) > 0:
+            print('Recebido: ' + str(buf) + '\n')
+            respond('Mensagem Recebida pelo Rasp com Sucesso!')
+
+
 def readTemperature():
+    global semaforo
+    global cooler_status
+    global temperatura_atual
     while True:
-        temperatura = randint(25,33)
-        lista_mensagens.append(temperatura)
-        global cooler_status
-        if temperatura > 30:
+        print('readTemperature Deseja adquirir o semáforo')
+        semaforo.acquire()
+        print('readTemperature Adquiriu o semáforo')
+        temperatura_atual = randint(25, 42)
+        semaforo.release()
+        print('readTemperature Liberou o semáforo')
+        if temperatura_atual > 30:
             cooler_status = 1
             print('Ativando cooler')
         else:
             cooler_status = 0
             print('Desativando cooler')
         time.sleep(5)
-        
+
+
 def sendData():
+    global temperatura_atual
+    global semaforo
     while True:
         time.sleep(4)
-        if len(lista_mensagens) > 0:
-            respond(str(lista_mensagens.pop()))
-            
+        if temperatura_atual != -999:
+            print('sendData Deseja adquirir o semáforo')
+            semaforo.acquire()
+            print('sendData Adquiriu o semáforo')
+            respond(str(temperatura_atual))
+            temperatura_atual = -999
+            semaforo.release()
+            print('sendData Liberou o semáforo')
+
+
 def coolerHandler():
     while True:
         time.sleep(4)
@@ -92,7 +114,8 @@ def coolerHandler():
             print('Ativando cooler')
         else:
             print('Desativando cooler')
-            
-#Thread 0 - Encontrar servidor na rede
+
+
+# Thread 0 - Encontrar servidor na rede
 serverFindThread = Thread(target=findServer)
 serverFindThread.start()
